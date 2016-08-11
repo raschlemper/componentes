75b21e8a73cd676ec4b5879d2bef22b7ee8938fd
Raschlemper <raschlemper@gmail.com>
2016-08-10 23:36:20 -0300

    add no sorter

--- table/dist/js/app-table-tpls.js
+++ table/dist/js/app-table-tpls.js
@@ -6,9 +6,9 @@ try {
 }
 module.run(['$templateCache', function($templateCache) {
   $templateCache.put('app-table.html',
-    '<table class="table table-striped table-bordered table-hover table-condensed"><thead><tr><th ng-repeat="column in columns track by $index" ng-click="orderColumn(listaFiltrada, column.value, reverse)" ng-class="{sortorder_asc  : !reverse && predicate === column.value, \n' +
+    '<table class="table table-striped table-bordered table-hover table-condensed"><thead><tr><th ng-repeat="column in columns track by $index" ng-click="orderColumn(listFiltered, column.value, reverse)" ng-class="{sortorder_asc  : !reverse && predicate === column.value, \n' +
     '                           sortorder_desc :  reverse && predicate === column.value, \n' +
-    '                           sortorder      :             predicate !== column.value}">{{column.label}}</th></tr></thead><tbody><tr ng-repeat="item in listaFiltrada = filterList(list, search) | \n' +
+    '                           sortorder      :             predicate !== column.value}" class={{column.headerClass}}>{{column.label}}</th></tr></thead><tbody><tr ng-repeat="item in listFiltered = filterList(list, search) | \n' +
     '                       orderBy : predicate : reverse | \n' +
     '                       limitTo : limitTo.value : start track by $index"><td ng-repeat="column in columns track by $index" compile="getColumn(item, column)"></td></tr></tbody></table>');
 }]);
